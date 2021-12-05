6e1f190c6f47ef1732aa0d720d6e2b4764e11aec
Kevin Batema <kevinbatema@gmail.com>
2021-12-05 16:21:10 +0100

    day 4; part 1 and 2

---
+++ day4/part1/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"aoc2021"
+	"log"
+	"strings"
+)
+
+func main() {
+	defer aoc2021.Time()()
+	input := aoc2021.GetInput()
+
+	answer := solve(input)
+	log.Printf("anwser: %v", answer)
+}
+
+func solve(input []string) int {
+	callNumbers, cards := getCardsFromInput(input)
+
+	for _, called := range callNumbers {
+		for _, c := range cards {
+			if c.mark(called) {
+				return called * c.unmarkedSum()
+			}
+		}
+	}
+
+	return -1
+}
+
+func newBingoCard(numbers []int) *bingoCard {
+	return &bingoCard{
+		numbers: numbers,
+		offset:  5,
+		bingo:   false,
+	}
+}
+
+type bingoCard struct {
+	numbers []int
+	offset  int
+
+	bingo bool
+}
+
+func (b *bingoCard) mark(calledNumber int) bool {
+	for i, n := range b.numbers {
+		if n == calledNumber {
+			b.numbers[i] = -1
+
+			if b.verifyRow(i / b.offset) {
+				b.bingo = true
+			}
+
+			if b.verifyCol(i % b.offset) {
+				b.bingo = true
+			}
+
+			break
+		}
+	}
+
+	return b.bingo
+}
+
+func (b *bingoCard) unmarkedSum() int {
+	sum := 0
+	for _, n := range b.numbers {
+		if n >= 0 {
+			sum += n
+		}
+	}
+
+	return sum
+}
+
+func (b *bingoCard) verifyRow(rowNum int) bool {
+	start := rowNum * b.offset
+	end := start + b.offset
+
+	for _, cell := range b.numbers[start:end] {
+		if cell >= 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (b *bingoCard) verifyCol(colNum int) bool {
+	for i := 0; i < b.offset; i++ {
+		if b.numbers[colNum+i*b.offset] >= 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getCardsFromInput(input []string) (numbers []int, cards []*bingoCard) {
+	numbers = aoc2021.AtoiSlice(strings.Split(input[0], ","))
+
+	cardNumbers := []int{}
+
+	for i := 2; i <= len(input); i++ {
+
+		if i == len(input) || input[i] == "" {
+			cards = append(cards, newBingoCard(cardNumbers))
+			cardNumbers = []int{}
+			continue
+		}
+
+		line := input[i]
+		parsedNumbers := []int{}
+		for _, s := range strings.Split(line, " ") {
+			if s != "" {
+				parsedNumbers = append(parsedNumbers, aoc2021.Atoi(s))
+			}
+		}
+
+		cardNumbers = append(cardNumbers, parsedNumbers...)
+	}
+
+	return numbers, cards
+}
---
+++ day4/part1/main_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"aoc2021"
+	"testing"
+)
+
+func TestSolve(t *testing.T) {
+	expectedAnswer := 4512
+	input := aoc2021.GetTestInput()
+	answer := solve(input)
+
+	if answer != expectedAnswer {
+		t.Errorf("expcted answer:  %v, got: %v", expectedAnswer, answer)
+	}
+}
+
+func TestMark(t *testing.T) {
+	for _, called := range []int{1, 2, 3, 4, 5, 6, 7, 25} {
+		card := testCard()
+		if card.numbers[called-1] != called {
+			t.Errorf("expected %v got %v", called, card.numbers[0])
+		}
+		card.mark(called)
+		if card.numbers[called-1] != -1 {
+			t.Errorf("expected -1 got %v", card.numbers[0])
+		}
+	}
+}
+
+func TestUnmarkedSum(t *testing.T) {
+	card := testCard()
+	card.numbers = []int{
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1,
+	}
+
+	if sum := card.unmarkedSum(); sum != 20 {
+		t.Errorf("expected sum to be 20, got: %v", sum)
+	}
+
+}
+func TestVerifyRow(t *testing.T) {
+	card := testCard()
+
+	for i := 0; i < 5; i++ {
+		if card.verifyRow(i) {
+			t.Errorf("should not be valid: row number: %v card: %v", i, card.numbers)
+		}
+	}
+	card.numbers[0] = -1
+	card.numbers[1] = -1
+	card.numbers[2] = -1
+	card.numbers[3] = -1
+	card.numbers[4] = -1
+
+	if !card.verifyRow(0) {
+		t.Errorf("should be valid: row number: %v card: %v", 0, card.numbers)
+	}
+
+	card = testCard()
+	card.numbers[5] = -1
+	card.numbers[6] = -1
+	card.numbers[7] = -1
+	card.numbers[8] = -1
+	card.numbers[9] = -1
+
+	if !card.verifyRow(1) {
+		t.Errorf("should be valid: row number: %v card: %v", 0, card.numbers)
+	}
+
+}
+
+func TestVerifyCol(t *testing.T) {
+	card := testCard()
+
+	for i := 0; i < 5; i++ {
+		if card.verifyCol(i) {
+			t.Errorf("should not be valid: row number: %v card: %v", i, card.numbers)
+		}
+	}
+	card.numbers[0] = -1
+	card.numbers[5] = -1
+	card.numbers[10] = -1
+	card.numbers[15] = -1
+	card.numbers[20] = -1
+	if !card.verifyCol(0) {
+		t.Errorf("should be valid: col number: %v card: %v", 0, card.numbers)
+	}
+
+	card = testCard()
+	card.numbers[2] = -1
+	card.numbers[7] = -1
+	card.numbers[12] = -1
+	card.numbers[17] = -1
+	card.numbers[22] = -1
+	if !card.verifyCol(2) {
+		t.Errorf("should be valid: col number: %v card: %v", 0, card.numbers)
+	}
+}
+
+func testCard() *bingoCard {
+	numbers := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+	}
+	return newBingoCard(numbers)
+}
